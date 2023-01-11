b4dfa79647bdf01efb7cc8d838600902c85a9b74
Ben Garski <ben.garski@outlook.com>
2023-01-11 11:34:02 -0700

    added clock minutes and seconds and reordered columns

--- dagster_project/assets.py
+++ dagster_project/assets.py
@@ -7,9 +7,7 @@ from google.cloud import bigquery
 @asset(required_resource_keys={"cfb_token"})
 def get_play_by_play_data(context):
     url = "https://api.collegefootballdata.com/plays"
-    headers = {
-        "Authorization": context.resources.cfb_token
-    }
+    headers = {"Authorization": context.resources.cfb_token}
     df = pd.DataFrame()
     for week in range(1, 17):
         params = {"year": 2022, "seasonType": 'regular', 'week': week}
@@ -25,14 +23,46 @@ def get_play_by_play_data(context):
     return df
 
 @asset
-def clean_play_by_play_data(context, get_play_by_play_data):
+def clean_play_by_play_data(get_play_by_play_data):
     df = get_play_by_play_data.reset_index(drop=True)
-    context.log.debug(df.game_id[0])
     df["game_id"] = df["game_id"].astype(int).astype(str)
-    context.log.debug(df.game_id[0])
-    df["clock"] = df["clock"].apply(lambda x: f"{x['minutes']}:{x['seconds']}")
+    df["clock_minutes"] = df["clock"].apply(lambda x: f"{x['minutes']}").astype(int)
+    df["clock_seconds"] = df["clock"].apply(lambda x: f"{x['seconds']}").astype(int)
+    df = df.drop('clock', axis=1)
     df["ppa"] = df["ppa"].astype(float)
     df["wallclock"] = pd.to_datetime(df["wallclock"])
+    col_order = [
+        "week",
+        "game_id",
+        "drive_id",
+        "id",
+        "home",
+        "away",
+        "offense",
+        "defense",
+        "offense_conference",
+        "defense_conference",
+        "offense_score",
+        "defense_score",
+        "offense_timeouts",
+        "defense_timeouts",
+        "period",
+        "drive_number",
+        "play_number",
+        "yard_line",
+        "yards_to_goal",
+        "down",
+        "distance",
+        "yards_gained",
+        "wallclock",
+        "clock_minutes",
+        "clock_seconds",
+        "scoring",
+        "play_type",
+        "play_text",
+        "ppa"
+    ]
+    df = df.reindex(columns=col_order)
     return df
 
 
@@ -42,34 +72,35 @@ def load_play_by_play_data(context, clean_play_by_play_data):
     table_id = "bigquerytest-373818.football.playbyplay"
     job_config = bigquery.LoadJobConfig(
         schema=[
-            bigquery.SchemaField("id", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("offense", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("offense_conference", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("defense", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("defense_conference", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("home", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("away", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("offense_score", bigquery.enums.SqlTypeNames.INT64),
-            bigquery.SchemaField("defense_score", bigquery.enums.SqlTypeNames.INT64),
+            bigquery.SchemaField("week", bigquery.enums.SqlTypeNames.INT64),
             bigquery.SchemaField("game_id", bigquery.enums.SqlTypeNames.STRING),
             bigquery.SchemaField("drive_id", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("drive_number", bigquery.enums.SqlTypeNames.INT64),
-            bigquery.SchemaField("play_number", bigquery.enums.SqlTypeNames.INT64),
-            bigquery.SchemaField("period", bigquery.enums.SqlTypeNames.INT64),
-            bigquery.SchemaField("clock", bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField("id", bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField("home", bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField("away", bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField("offense", bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField("defense", bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField("offense_conference", bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField("defense_conference", bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField("offense_score", bigquery.enums.SqlTypeNames.INT64),
+            bigquery.SchemaField("defense_score", bigquery.enums.SqlTypeNames.INT64),
             bigquery.SchemaField("offense_timeouts", bigquery.enums.SqlTypeNames.INT64),
             bigquery.SchemaField("defense_timeouts", bigquery.enums.SqlTypeNames.INT64),
+            bigquery.SchemaField("period", bigquery.enums.SqlTypeNames.INT64),
+            bigquery.SchemaField("drive_number", bigquery.enums.SqlTypeNames.INT64),
+            bigquery.SchemaField("play_number", bigquery.enums.SqlTypeNames.INT64),
             bigquery.SchemaField("yard_line", bigquery.enums.SqlTypeNames.INT64),
             bigquery.SchemaField("yards_to_goal", bigquery.enums.SqlTypeNames.INT64),
             bigquery.SchemaField("down", bigquery.enums.SqlTypeNames.INT64),
             bigquery.SchemaField("distance", bigquery.enums.SqlTypeNames.INT64),
-            bigquery.SchemaField("scoring", bigquery.enums.SqlTypeNames.BOOL),
             bigquery.SchemaField("yards_gained", bigquery.enums.SqlTypeNames.INT64),
+            bigquery.SchemaField("wallclock", bigquery.enums.SqlTypeNames.DATETIME),
+            bigquery.SchemaField("clock_minutes", bigquery.enums.SqlTypeNames.INT64),
+            bigquery.SchemaField("clock_seconds", bigquery.enums.SqlTypeNames.INT64),
+            bigquery.SchemaField("scoring", bigquery.enums.SqlTypeNames.BOOL),
             bigquery.SchemaField("play_type", bigquery.enums.SqlTypeNames.STRING),
             bigquery.SchemaField("play_text", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("ppa", bigquery.enums.SqlTypeNames.FLOAT64),
-            bigquery.SchemaField("wallclock", bigquery.enums.SqlTypeNames.DATETIME),
-            bigquery.SchemaField("week", bigquery.enums.SqlTypeNames.INT64)
+            bigquery.SchemaField("ppa", bigquery.enums.SqlTypeNames.FLOAT64)
         ],
         write_disposition="WRITE_TRUNCATE"
     )
